1da09b481c45645672434e890e3c1399d0ae01e4
MehmetKaan96 <mehmetkaan1996@gmail.com>
2022-07-27 22:36:57 +0300

    added features

--- ListApp/Controllers/ViewController.swift
+++ ListApp/Controllers/ViewController.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController{
     
     var alertController = UIAlertController()
     
     @IBOutlet weak var tableView: UITableView!
     
     
-    var data = [String]()
+    var data = [NSManagedObject]()
     
     override func viewDidLoad(){
         super.viewDidLoad()
@@ -22,32 +23,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.delegate = self
         tableView.dataSource = self
+        fetch()
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell",for: indexPath)
-        
-        cell.textLabel?.text = data[indexPath.row]
-        
-        return cell
-        
-    }
-    
     @IBAction func addBarButton(_ sender: UIBarButtonItem){
         presentAddAlert()
     }
     
     func presentAddAlert(){
         
-        presentAlert(title: "Yeni Eleman Ekle", message: nil, preferredStyle: UIAlertController.Style.alert, defaultButtonTitle: "Ekle", cancelButtonTitle: "Vazgeç", isTextFieldAvailable: true) { _ in
+        presentAlert(title: "Yeni Eleman Ekle", message: nil, preferredStyle: UIAlertController.Style.alert, defaultButtonTitle: "Ekle", cancelButtonTitle: "Vazgeç", isTextFieldAvailable: true) { [self] _ in
             let textfield = self.alertController.textFields?.first?.text
             if textfield != ""{
-                self.data.append((textfield)!)
-                self.tableView.reloadData()
+                
+                let appDelegate = UIApplication.shared.delegate as? AppDelegate
+                
+                let managedObjectContext = appDelegate?.persistentContainer.viewContext
+                
+                let entity = NSEntityDescription.entity(forEntityName: "ListItem",
+                                                        in: managedObjectContext!)
+                
+                let listItem = NSManagedObject(entity: entity!,
+                                               insertInto: managedObjectContext)
+                
+                listItem.setValue(textfield, forKey: "title")
+                
+                try? managedObjectContext?.save()
+                
+                self.fetch()//fetch data from core data
             }else{
                 self.presentWarningAlert()
             }
@@ -101,10 +103,97 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                      defaultButtonTitle: "Tamam",
                      cancelButtonTitle: "Vazgeç",
                      isTextFieldAvailable: false) { _ in
-            self.data.removeAll()
+            //self.data.removeAll()
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            
+            let managedObjectContext = appDelegate?.persistentContainer.viewContext
+            
+            self.deleteData()
+            
             self.tableView.reloadData()
         }
     }
     
+    func deleteData(){
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        let managedObjectContext = appDelegate?.persistentContainer.viewContext
+        
+        for object in data{
+            managedObjectContext?.delete(object)
+        }
+        
+        try! managedObjectContext?.save()
+        
+    }
+    
+    func fetch(){
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        let managedObjectContext = appDelegate?.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ListItem")
+        
+        data = try! managedObjectContext!.fetch(fetchRequest)
+        
+        tableView.reloadData()
+    }
+    
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell",for: indexPath)
+        let listItem = data[indexPath.row]
+        
+        cell.textLabel?.text = listItem.value(forKey: "title") as? String
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: UIContextualAction.Style.normal,
+                                              title: "Sil") { [self] _, _, _ in
+            
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            
+            let managedObjectContext = appDelegate?.persistentContainer.viewContext
+            
+            managedObjectContext?.delete(self.data[indexPath.row])
+            try? managedObjectContext?.save()
+            self.fetch()
+        }
+        deleteAction.backgroundColor = UIColor.systemRed
+        
+        let editAction = UIContextualAction(style: .normal, title: "Düzenle") { _, _, _ in
+            self.presentAlert(title: "Düzenle", message: nil, preferredStyle: UIAlertController.Style.alert, defaultButtonTitle: "Ekle", cancelButtonTitle: "Vazgeç", isTextFieldAvailable: true) { _ in
+                let textfield = self.alertController.textFields?.first?.text
+                if textfield != ""{
+                    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+                    
+                    let managedObjectContext = appDelegate?.persistentContainer.viewContext
+                    
+                    self.data[indexPath.row].setValue(textfield, forKey: "title")
+                    
+                    if managedObjectContext!.hasChanges{
+                        try? managedObjectContext?.save()
+                    }
+                    self.tableView.reloadData()
+                }else{
+                    self.presentWarningAlert()
+                }
+            }
+        }
+        
+        let config = UISwipeActionsConfiguration(actions: [deleteAction,editAction])
+        
+        return config
+    }
+    
+}
